665e543cf69b0f83128e86609484a8ce114a77a1
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-07-14 21:09:51 -0300

    finalizando as validações de tasks

--- src/main/java/com/maua/roubit/shared/domain/entities/Tasks.java
+++ src/main/java/com/maua/roubit/shared/domain/entities/Tasks.java
@@ -2,6 +2,10 @@ package com.maua.roubit.shared.domain.entities;
 
 import java.util.UUID;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -13,25 +17,32 @@ public class Tasks {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "task_id", nullable = false, unique = true, length = 36)
+    @NotNull(message = "Task ID não pode ser null")
     private UUID taskId;
 
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @NotNull(message = "User ID (Tasks) não poder ser null")
     private Users userId;
 
     @Column(length = 50, nullable = false)
+    @NotBlank(message = "Task name não pode estar em branco")
     private String name;
 
     @Column()
+    @Size(max = 255, message = "Task description pode ter no máximo 255 caracteres")
     private String description;
 
     @Column()
+    @Size(max = 255, message = "Task image pode ter no máximo 255 caracteres")
     private String image;
 
     @Column()
+    @PositiveOrZero(message = "Task chronometer deve ser maior ou igual a 0")
     private Long chronometer;
 
     @Column()
+    @PositiveOrZero(message = "Task time deve ser maior ou igual a 0")
     private Long time;
 }
